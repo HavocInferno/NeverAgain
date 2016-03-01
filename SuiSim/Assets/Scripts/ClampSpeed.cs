dba0565dd2b9d50527f64cf2477f8af9e4832342
Paul Preißner (TUM) <paul@preissner-muc.de>
2016-03-01 14:47:58 +0100

    omylord

---
+++ SuiSim/Assets/Scripts/ClampSpeed.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClampSpeed : MonoBehaviour {
+    Rigidbody rb;
+    static float maxspeed = 60.0f;
+    // Update is called once per frame
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	void FixedUpdate () {
+        if (rb.velocity.magnitude > maxspeed)
+            rb.velocity = rb.velocity.normalized * maxspeed;
+	}
+}
